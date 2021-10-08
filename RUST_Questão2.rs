6b504cf461a4416c4c0bf968bf4f1b75df3d2593
Gabriel Ferreira <84155888+KING36122@users.noreply.github.com>
2021-10-08 17:03:16 -0400

    Questão2 RUST

--- RUST_Questão2.rs
+++ RUST_Questão2.rs
@@ -1,97 +1,37 @@
 fn main() {
-    let  mut A:bool;
-    let mut B:bool;
-    let mut result:bool;
-    
-    //true = 1 e false = 0
+    let mut A:u8;
+    let mut B:u8;
+    let mut X:u8;
+
+    //UTILIZEI DE DOIS EXEMPLOS COM AS PORTAS LÓGICAS NOT E NAND
     
     //OPERAÇÕES NOT
-    A = true;
-    result = !(!A);
-    println!("NOT(NOT A) (quando A = 1): {}\n",result);
+    A = 0;
+    X = !(!A);
+    println!("NOT(NOT A): {}",X);
     
-    A = false;
-    println!("NOT(NOT A) (quando A = 0): {}\n",result);
-    
+    A = 1;
+    X = !(!A);
+    println!("NOT(NOT A): {}",X);
     
     //OPERAÇÕES NAND
-    A = false;
-    B = false;
-    result = !(!(A&B));
-    println!("NOT(NOT(A AND B)) (quando A = 0 e B = 0): {}\n",result);
+    A = 0;
+    B = 0;
+    X = !(!(A&B));
+    println!("\nNOT(NOT(A AND B)): {}",X);
     
-    A = false;
-    B = true;
-    result = !(!(A&B));
-    println!("NOT(NOT(A AND B)) (quando A = 0 e B = 1): {}\n",result);
+    A = 0;
+    B = 1;
+    X = !(!(A&B));
+    println!("NOT(NOT(A AND B)): {}",X);
     
-    A = true;
-    B = false;
-    result = !(!(A&B));
-    println!("NOT(NOT(A AND B)) (quando A = 1 e B = 0): {}\n",result);
+    A = 1;
+    B = 0;
+    X = !(!(A&B));
+    println!("NOT(NOT(A AND B)): {}",X);
     
-    A = true;
-    B = true;
-    result = !(!(A&B));
-    println!("NOT(NOT(A AND B)) (quando A = 1 e B = 1): {}\n",result);
-    
-    A = false;
-    B = false;
-    result = !(!(A|B));
-    println!("NOT(NOT(A OR B)) (quando A = 0 e B = 0): {}\n",result);
-    
-    A = false;
-    B = true;
-    result = !(!(A|B));
-    println!("NOT(NOT(A OR B)) (quando A = 0 e B = 1): {}\n",result);
-    
-    A = true;
-    B = false;
-    result = !(!(A|B));
-    println!("NOT(NOT(A OR B)) (quando A = 1 e B = 0): {}\n",result);
-    
-    A = true;
-    B = true;
-    result = !(!(A|B));
-    println!("NOT(NOT(A OR B)) (quando A = 1 e B = 1): {}\n",result);
-    
-    A = false;
-    B = false;
-    result = !(A&B);
-    println!("NOT(A AND B) (quando A = 0 e B = 0): {}\n",result);
-    
-    A = false;
-    B = true;
-    result = !(A&B);
-    println!("NOT(A AND B) (quando A = 0 e B = 1): {}\n",result);
-    
-    A = true;
-    B = false;
-    result = !(A&B);
-    println!("NOT(A AND B) (quando A = 1 e B = 0): {}\n",result);
-    
-    A = true;
-    B = true;
-    result = !(A&B);
-    println!("NOT(A AND B) (quando A = 1 e B = 1): {}\n",result);
-    
-    A = false;
-    B = false;
-    result = !(A|B);
-    println!("NOT(A OR B) (quando A = 0 e B = 0): {}\n",result);
-    
-    A = false;
-    B = true;
-    result = !(A|B);
-    println!("NOT(A OR B) (quando A = 0 e B = 1): {}\n",result);
-    
-    A = true;
-    B = false;
-    result = !(A|B);
-    println!("NOT(A OR B) (quando A = 1 e B = 0): {}\n",result);
-    
-    A = true;
-    B = true;
-    result = !(A|B);
-    println!("NOT(A OR B) (quando A = 1 e B = 1): {}\n",result);
+    A = 1;
+    B = 1;
+    X = !(!(A&B));
+    println!("NOT(NOT(A AND B)): {}",X);
 }
