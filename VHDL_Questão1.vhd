d9cb57c0d1ed7e40ee24188c4f6fbb5d063d01d7
Gabriel Ferreira <84155888+KING36122@users.noreply.github.com>
2021-10-08 17:14:57 -0400

    Questão1 VHDL

--- VHDL_Questão1.vhd
+++ VHDL_Questão1.vhd
@@ -9,10 +9,6 @@ end circuito;
 
 architecture funcCircuito of circuito is
 Begin
-    --UTILIZEI DUAS PORTAS LÓGICAS COMO EXEMPLO: NOT E NAND
-    --Negando a negação de A
-    X <= NOT(NOT(A));
-
     --Negando o NAND
     X <= NOT(NOT(A AND B));
 end funcCircuito;
